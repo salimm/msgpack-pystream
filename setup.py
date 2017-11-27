628ecce34828a03f7130faf41621843e0aa97046
salimm <salimm@cs.pitt.edu>
2017-11-27 11:19:36 -0500

    updated url

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
   author = 'Salim Malakouti',
   author_email = 'salim.malakouti@gmail.com',
   url = 'https://github.com/salimm/msgpack-pystream', # use the URL to the github repo
-  download_url = 'https://github.com/salimm/msgpack-pystream/archive/1.0.tar.gz', # I'll explain this in a second
+  download_url = 'http://github.com/salimm/msgpack-pystream/archive/1.0.tar.gz', # I'll explain this in a second
   keywords = ['python','msgpack','serialization','binary','fast'], # arbitrary keywords
   classifiers = [],
 )

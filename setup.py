9863681cbfa2eb6f2a271012453b5e6ca6f9f5bd
salimm <salimm@cs.pitt.edu>
2018-03-08 08:36:35 -0500

    changed packaging and version updated setup.py to 1.2.3 and added include_package_data

--- setup.py
+++ setup.py
@@ -4,20 +4,21 @@
 # from glob import glob
 # from distutils.command.sdist import sdist
 from setuptools import setup, Extension
+import msgpackstream
 
 
 module1 = Extension('mpstream_cunpacker',
                     sources = [
-                               'msgpackpyc/unpacker.cpp',
-                               'msgpackpyc/unpackerapi.cpp',
-                               'msgpackpyc/ParserInfo.cpp',                               
-                               'msgpackpyc/Event.cpp',
-                               'msgpackpyc/ExtType.cpp',
-                               'msgpackpyc/HeaderUtil.cpp',
-                               'msgpackpyc/ParserState.cpp',
+                               'msgpackpyc/src/unpacker.cpp',
+                               'msgpackpyc/src/unpackerapi.cpp',
+                               'msgpackpyc/src/ParserInfo.cpp',                               
+                               'msgpackpyc/src/Event.cpp',
+                               'msgpackpyc/src/ExtType.cpp',
+                               'msgpackpyc/src/HeaderUtil.cpp',
+                               'msgpackpyc/src/ParserState.cpp',
                                ],
 #                     libraries=['libs/'],
-                    include_dirs=['include/']
+                    include_dirs=['msgpackpyc/include/']
                      
                     )
 
@@ -25,13 +26,15 @@ module1 = Extension('mpstream_cunpacker',
 setup(
   name = 'msgpackstream',
   packages = ['msgpackstream','msgpackstream.backend', 'msgpackstream.backend.python','msgpackstream.backend.pyc'], # this must be the same as the name above
-  version = '1.2.2',
+  version = '1.2.3',
+  include_package_data = True,
+  exclude_package_data = ['test','samples','dist','build','msgpackstream.egg-info'],
   description = 'A SAX-like MessagegPack library in python to deserialize messages from an input stream',
   author = 'Salim Malakouti',
   author_email = 'salim.malakouti@gmail.com',
   license = 'MIT',
   url = 'https://github.com/salimm/msgpack-pystream', # use the URL to the github repo
-  download_url = 'http://github.com/salimm/msgpack-pystream/archive/1.2.2.tar.gz', # I'll explain this in a second
+  download_url = 'http://github.com/salimm/msgpack-pystream/archive/1.2.3.tar.gz', # I'll explain this in a second
   keywords = ['python','msgpack','serialization','binary','fast'], # arbitrary keywords
   classifiers = ['Programming Language :: Python'],
   install_requires=['msgpack_python','enum34'],

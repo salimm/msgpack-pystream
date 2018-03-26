d786fbae2ebc5188f24a818c23a0aa0431eda68f
salimm <salimm@cs.pitt.edu>
2018-03-26 16:55:26 -0400

    fixed memory leak problem for lists and updated setup.py

--- setup.py
+++ setup.py
@@ -27,13 +27,13 @@ module1 = Extension('mpstream_cunpacker',
 setup(
   name = 'msgpackstream',
   packages = ['msgpackstream','msgpackstream.backend', 'msgpackstream.backend.python','msgpackstream.backend.pyc'], # this must be the same as the name above
-  version = '1.2.6',
+  version = '1.2.7',
   description = 'A SAX-like MessagegPack library in python to deserialize messages from an input stream',
   author = 'Salim Malakouti',
   author_email = 'salim.malakouti@gmail.com',
   license = 'MIT',
   url = 'https://github.com/salimm/msgpack-pystream', # use the URL to the github repo
-  download_url = 'http://github.com/salimm/msgpack-pystream/archive/1.2.6.tar.gz', # I'll explain this in a second
+  download_url = 'http://github.com/salimm/msgpack-pystream/archive/1.2.7.tar.gz', # I'll explain this in a second
   keywords = ['python','msgpack','serialization','binary','fast'], # arbitrary keywords
   classifiers = ['Programming Language :: Python'],
   install_requires=['msgpack_python','msgpack','enum34'],

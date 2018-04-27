bb7dbb013e38576261f23521b25f15c67b5622ea
salimm <salimm@cs.pitt.edu>
2018-04-27 10:45:52 -0400

    updated setup.py to  2.0.4

--- setup.py
+++ setup.py
@@ -19,13 +19,13 @@ module1 = Extension('mpstream_cunpacker',
 setup(
   name = 'msgpackstream',
   packages = ['msgpackstream','msgpackstream.backend', 'msgpackstream.backend.python','msgpackstream.backend.pyc'], # this must be the same as the name above
-  version = '2.0.3',
+  version = '2.0.4',
   description = 'A SAX-like MessagegPack library in python to deserialize messages from an input stream',
   author = 'Salim Malakouti',
   author_email = 'salim.malakouti@gmail.com',
   license = 'MIT',
   url = 'https://github.com/salimm/msgpack-pystream', # use the URL to the github repo
-  download_url = 'http://github.com/salimm/msgpack-pystream/archive/2.0.3.tar.gz', # I'll explain this in a second
+  download_url = 'http://github.com/salimm/msgpack-pystream/archive/2.0.4.tar.gz', # I'll explain this in a second
   keywords = ['python','msgpack','serialization','binary','fast'], # arbitrary keywords
   classifiers = ['Programming Language :: Python'],
   install_requires=['msgpack_python','msgpack','enum34'],
